694befc513600dc76159fdd5827e89767fd3a322
lucasferrre <lucasferreirasasuke@gmail.com>
2025-09-04 11:14:28 -0300

    Atividade_1

--- server.js
+++ server.js
@@ -17,31 +17,42 @@ app.get('/soma/:numUm/:numDois', async (req, res) => {
 });
 
 app.get('/subtracao/:numUm/:numDois', async (req, res) => {
-    try {
-        const dadosJson = await lerJson('./produtos.json');
-        res.status(200).json(dadosJson);
-    } catch (error) {
+    try{
+        const numUm = parseFloat(req.params.numUm);
+        const numDois = parseFloat(req.params.numDois);
+        const resut = numUm - numDois;
+        return res.status(200).json(`resultado da soma: ${resut}`);
+    }catch(error){
+        res.status(500).json({ erro: error.message });
+    }
+    
+});
+
+app.get('/multiplicacao/:numUm/:numDois', async (req, res) => {
+    try{
+        const numUm = parseFloat(req.params.numUm);
+        const numDois = parseFloat(req.params.numDois);
+        const resut = numUm * numDois;
+        return res.status(200).json(`resultado da soma: ${resut}`);
+    }catch(error){
         res.status(500).json({ erro: error.message });
     }
 });
 
 app.get('/divisao/:numUm/:numDois', async (req, res) => {
-    try {
-        const dadosJson = await lerJson('./produtos.json');
-        res.status(200).json(dadosJson);
-    } catch (error) {
+    try{
+        const numUm = parseFloat(req.params.numUm);
+        const numDois = parseFloat(req.params.numDois);
+        const resut = numUm / numDois;
+        if(numDois === 0) {
+            return res.status(400).json({ erro: "Divisão por zero não é permitida." });
+        }
+        return res.status(200).json(`resultado da soma: ${resut}`);
+    }catch(error){
         res.status(500).json({ erro: error.message });
     }
 });
 
-app.get('/multiplicacao/:numUm/:numDois', async (req, res) => {
-    try {
-        const dadosJson = await lerJson('./produtos.json');
-        res.status(200).json(dadosJson);
-    } catch (error) {
-        res.status(500).json({ erro: error.message });
-    }
-});
 
 app.use((req, res) => {
     res.status(404).send('Página não encontrada!');
