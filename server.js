d3f3b1d02ec5e16f3a628e33dc259b62bd11a1d1
lucasferrre <lucasferreirasasuke@gmail.com>
2025-09-18 09:15:57 -0300

    finalizando a atividade_3

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const app = require('express')();
-const fs = require('fs/promises');
 const PORT = 2900;
+
+// exercicio 3
+// Criando uma rota /operacao/:tipo onde tipo é a operação matemática (soma, subtracao, multiplicacao, divisao).
 
 app.get('/operacao/:tipo', (req, res) => {
     const { tipo } = req.params;
@@ -11,30 +13,25 @@ app.get('/operacao/:tipo', (req, res) => {
     let resultado;
   
     if (isNaN(n1) || isNaN(n2)) {
-      return res.status(400).json({ erro: 'Os parâmetros "numUm" e "numDois" são obrigatórios e devem ser números.' });
+      return res.status(400).send({ erro: 'Os parâmetros "numUm" e "numDois" são obrigatórios e devem ser números.' });
     }
-  
-    switch (tipo) {
-      case 'soma':
+
+    if(tipo === 'soma') {
         resultado = n1 + n2;
-        break;
-      case 'subtracao':
+    } else if(tipo === 'subtracao') {
         resultado = n1 - n2;
-        break;
-      case 'multiplicacao':
+    } else if(tipo === 'multiplicacao') {
         resultado = n1 * n2;
-        break;
-      case 'divisao':
-         if (n2 === 0) {
-          return res.status(400).json({ erro: 'Não é possível dividir por zero.' });
+    } else if(tipo === 'divisao') {
+        if(n2 === 0) {
+            return res.status(400).json({ erro: 'Divisão por zero não é permitida.' });
         }
         resultado = n1 / n2;
-        break;
-      default:
-        return res.status(400).json({ erro: 'Operação inválida. Use: soma, subtracao, multiplicacao, divisao.' });
+    } else {
+        return res.status(400).json({ erro: 'Tipo de operação inválido. Use "soma", "subtracao", "multiplicacao" ou "divisao".' });
     }
   
-    res.json({ resultado: resultado });
+    return res.status(200).send(`O resutado da ${tipo} é: ${resultado}`);
   });
 
 
