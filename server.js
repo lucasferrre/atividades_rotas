67f935a09c3cacb7e08427f3695bd29bd1f828e3
lucasferrre <lucasferreirasasuke@gmail.com>
2025-09-18 08:24:05 -0300

    fazendo a atividade_2

--- server.js
+++ server.js
@@ -3,17 +3,44 @@ const fs = require('fs/promises');
 const PORT = 2900;
 
 
-app.get('/calculadora/operacao:/numUm:/numDois:', async (req, res) => {
+app.get('/calculadora/:operacao/:numUm/:numDois', async (req, res) => {
     try {
-        const { valor } = req.query;
-        const dadosJson = await lerJson('./produtos.json');
-        const dadosFiltrados = await filtroPorValor(dadosJson, valor);
-        res.status(200).json(dadosFiltrados);
+        const { operacao, numUm, numDois } = req.query;
+        const n1 = parseFloat(numUm);
+        const n2 = parseFloat(numDois);
+        let result;
+
+        if (isNaN(n1) || isNaN(n2)) {
+            return res.status(400).send('Erro: Parâmetros "numUm" e "numDois" devem ser números válidos.');
+        }
+
+        switch (operacao) {
+            case 'soma':
+                result = n1 + n2;
+                break;
+            case 'subtracao':
+                result = n1 - n2;
+                break;
+            case 'multiplicacao':
+                result = n1 * n2;
+                break;
+            case 'divisao':
+                if (n2 === 0) {
+                    return res.status(400).send('Erro: Divisão por zero não é permitida.');
+                }
+                result = n1 / n2;
+                break;
+            default:
+                return res.status(400).send('Erro: Operação inválida. Use soma, subtracao, multiplicacao ou divisao.');
+        }
+
+        return res.status(200).send(`resultado da soma: ${result}`);
+
     } catch (error) {
         res.status(500).json({ erro: error.message });
     }
-});
 
+});
 
 app.use((req, res) => {
     res.status(404).send('Página não encontrada!');
