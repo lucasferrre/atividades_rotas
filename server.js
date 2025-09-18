b8c476bcd2a3302dc4522db8ebb2f2ecf016c3c2
lucasferrre <lucasferreirasasuke@gmail.com>
2025-09-18 08:07:01 -0300

    fazendo a atividade_2

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const fs = require('fs/promises');
 const PORT = 2900;
 
 
+app.get('/calculadora/operacao:/numUm:/numDois:', async (req, res) => {
+    try {
+        const { valor } = req.query;
+        const dadosJson = await lerJson('./produtos.json');
+        const dadosFiltrados = await filtroPorValor(dadosJson, valor);
+        res.status(200).json(dadosFiltrados);
+    } catch (error) {
+        res.status(500).json({ erro: error.message });
+    }
+});
+
+
 app.use((req, res) => {
     res.status(404).send('Página não encontrada!');
 });
